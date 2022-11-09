0c816d394e0df8e833006f9312cda21dc91a0c2d
puttisit <puttisit.name@hotmail.com>
2022-11-09 15:13:40 +0700

    add new page

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const chalk = require('chalk');
 const debug = require('debug');
 const morgan = require('morgan');
 const app = express();
-const port = 3000;
+const port = 5000;
+const path = require('path')
 
 app.use(morgan('combined'));
-
+app.use(express.static(path.join(__dirname,"/public/")))
 app.get("/",(req,res) =>{
 
-    res.send('hello Word noom');
+    res.send('hello Word ss!!');
    
 
 })   //จัดการ reques ด้วยหน้า /
